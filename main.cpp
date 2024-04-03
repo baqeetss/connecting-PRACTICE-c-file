d23de3d8fef3fde5c2dfa02b7dcf75c12a2b4f43
baqeets <165313291+baqeetss@users.noreply.github.com>
2024-04-04 00:11:38 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#pragma comment(lib, "urlmon.lib")
+
+
+void CONNECTING() {
+	std::cout << "c";
+	Sleep(150);
+	std::cout << "o";
+	Sleep(150);
+	std::cout << "n";
+	Sleep(150);
+	std::cout << "n";
+	Sleep(150);
+	std::cout << "e";
+	Sleep(150);
+	std::cout << "c";
+	Sleep(150);
+	std::cout << "t";
+	Sleep(150);
+	std::cout << "i";
+	Sleep(150);
+	std::cout << "n";
+	Sleep(150);
+	std::cout << "g";
+	Sleep(150);
+	std::cout << ".";
+	Sleep(150);
+	std::cout << ".";
+	Sleep(150);
+	std::cout << "." << std::endl;
+
+	system("cls");
+
+	
+
+	
+
+
+}
+void LOADING() {
+	std::cout << "l";
+	Sleep(150);
+	std::cout << "o";
+	Sleep(150);
+	std::cout << "a";
+	Sleep(150);
+	std::cout << "d";
+	Sleep(150);
+	std::cout << "i";
+	Sleep(150);
+	std::cout << "n";
+	Sleep(150);
+	std::cout << "g" << std::endl;
+	Sleep(150);
+	system("cls");
+}
+void LOADINGLOL() {
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-";
+	Sleep(1000);
+	std::cout << "-" << std::endl << std::endl << std::endl;
+	Sleep(1000);
+	
+
+}
+void SUCCESFUL() {
+	std::cout << "[+] SUCCESFULLY LOADED! " << std::endl;
+	Sleep(1000);
+	
+
+}
+
+
+
+
+
+
+int main() {
+	CONNECTING();
+	CONNECTING();
+	CONNECTING();
+	Sleep(1000);
+
+	LOADING();
+	LOADING();
+	LOADING();
+
+	
+	Sleep(1000);
+
+	SUCCESFUL();
+
+
+
+
+
+
+	system("pause");
+	system("cls");
+	return main();
+}
+
+
